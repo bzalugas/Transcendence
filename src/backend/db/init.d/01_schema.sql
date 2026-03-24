2032c067227a09aa53d4191a13f4683814270677
bazaluga <bazaluga@student.42.fr>
2026-03-24 14:07:54 +0100

    Working again DB container (important changes)
    WHAT CHANGES for DB:
    - The sql files and the script are now located in the
    src/backend/db/init.d folder.
    - For the moment, we finally use the POSTGRES_DB env variable
    - It no longer uses the script. It is still in the folder if we need it later.

--- src/backend/db/init.d/01_schema.sql
+++ src/backend/db/init.d/01_schema.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE "$DB_NAME";
+-- CREATE DATABASE ${DB_NAME};
 
-\c "$DB_NAME";
+-- \c ${DB_NAME};
 
 CREATE TABLE IF NOT EXISTS user_credentials (
 name    varchar(80),
--- src/backend/db/script/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-createdb -U "$POSTGRES_USER" "$DB_NAME"
-psql -U "$POSTGRES_USER" -d "$DB_NAME" < transcendence_db.sql
-psql -U "$POSTGRES_USER" "$DB_NAME" < db_dump.sql

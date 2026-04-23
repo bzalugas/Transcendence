47675a0e0a550e0590c2bac66ddb2438297a331f
BigBassata <liamcohen.pro@gmail.com>
2026-04-23 20:05:52 +0200

    remove duplicate jaccard/prisma files left from infra merge

--- src/backend/api/src/jaccard/jaccard.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JaccardController } from './jaccard.controller';
-import { JaccardService } from './jaccard.service';
-
-@Module({
-    controllers: [JaccardController],
-    providers: [JaccardService],
-})
-export class JaccardModule {}

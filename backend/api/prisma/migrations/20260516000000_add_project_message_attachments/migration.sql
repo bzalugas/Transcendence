bfbaa066c926231c450a1948cedf74a1fc9eccc9
bazaluga <bazaluga@student.42.fr>
2026-05-16 18:26:45 +0200

    Clean prisma migrations

--- backend/api/prisma/migrations/20260516000000_add_project_message_attachments/migration.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "Attachment" ADD COLUMN "projectMessageId" INTEGER;
-
-ALTER TABLE "Attachment"
-ADD CONSTRAINT "Attachment_projectMessageId_fkey"
-FOREIGN KEY ("projectMessageId") REFERENCES "ProjectMessage"("id")
-ON DELETE CASCADE ON UPDATE CASCADE;
-
-CREATE INDEX "Attachment_projectMessageId_idx" ON "Attachment"("projectMessageId");
